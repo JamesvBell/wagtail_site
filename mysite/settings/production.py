ba7258d41db803e995b6c9d9810c606d7ac629cf
James Bell <jamesvbell@gmail.com>
2025-05-15 15:36:25 -0700

    adding secret key

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -3,6 +3,8 @@ DEBUG = False
 
 import os, dj_database_url
 from pathlib import Path               # NEW
+# Read the secret key from the Render environment
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Convert BASE_DIR to a Path if base.py left it as a string
 
@@ -10,7 +12,7 @@ if isinstance(BASE_DIR, str):     # new
     BASE_DIR = Path(BASE_DIR)     # new
 
 
-ALLOWED_HOSTS = ["YOUR-SERVICE.onrender.com"]  # add custom domain later
+
 
 # ---> Static files ---------------------------------------------
 STATIC_ROOT = BASE_DIR / "staticfiles"         # now works (Path / str)
